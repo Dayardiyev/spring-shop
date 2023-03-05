6e72ab661e674072d414809a3ba6b77cb52021a2
Alisher <dayardiev@gmail.com>
2023-03-05 12:23:38 +0600

    Optimization

--- src/main/java/dayardiyev/shop/controller/UsersController.java
+++ src/main/java/dayardiyev/shop/controller/UsersController.java
@@ -2,7 +2,6 @@ package dayardiyev.shop.controller;
 
 
 import dayardiyev.shop.model.User;
-import dayardiyev.shop.model.UserComparator;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -31,21 +30,11 @@ public class UsersController {
     ) {
         List<User> userList = new ArrayList<>();
 
+        if (from == null) from = Integer.MIN_VALUE;
+        if (to == null) to = Integer.MAX_VALUE;
         for (User user : USERS) {
-            if (from != null && to != null) {
-                if (from <= user.getAge() && to >= user.getAge()) userList.add(user);
-            } else if (from != null) {
-                if (from <= user.getAge()) userList.add(user);
-            } else if (to != null) {
-                if (to >= user.getAge()) userList.add(user);
-            } else {
-                List<User> users = new ArrayList<>(List.of(USERS));
-                users.sort(new UserComparator());
-                return users;
-            }
+            if (user.getAge() >= from && user.getAge() <= to) userList.add(user);
         }
-
-        userList.sort(new UserComparator());
         return userList;
     }
 
